0cd612df1ba5fb4b8a6dcd741d768630ba146939
zsorosz <oroszi.zsofi@gmail.com>
2023-07-18 11:38:05 +0200

    Add routes and nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import TaskHeader from './components/TaskHeader.vue';
 
 </script>
 
 <template>
+  <TaskHeader />
   <RouterView />
 </template>
 
