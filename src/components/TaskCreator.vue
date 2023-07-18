c52941dd644d27dd871590cb1e8a6c8d3ce3d808
zsorosz <oroszi.zsofi@gmail.com>
2023-07-18 12:09:27 +0200

    Add task creator, emit value on click

---
+++ src/components/TaskCreator.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from "vue";
+
+const emit = defineEmits(["create-task"])
+
+const task = ref("Task");
+
+const createTask = () => {
+    emit("create-task", task.value)
+}
+</script>
+
+<template>
+  <div class="input-wrap">
+    <input type="text" v-model="task" />
+    <button @click="createTask()">Create</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.input-wrap {
+  display: flex;
+  transition: 250ms ease;
+  border: 2px solid #41b080;
+
+  &:focus-within {
+    box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+      0 -2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+}
+</style>
--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,8 +1,23 @@
-<script setup></script>
+<script setup>
+  import { ref } from 'vue';
+  import {uid} from 'uid';
+  import TaskCreator from '../components/TaskCreator.vue';
+  const taskList = ref([]);
+
+  const createTaks = (task) => {
+    taskList.value.push({
+      id: uid(),
+      task,
+      isCompleted: null,
+      isEditing: null
+    })
+  }
+</script>
 
 <template>
   <main>
     <h1>Create Task</h1>
+    <TaskCreator @create-task="createTaks()" />
   </main>
 </template>
 
