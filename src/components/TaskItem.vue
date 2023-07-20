94934c45accb71ad637289e96d90c1a7452824ea
zsorosz <oroszi.zsofi@gmail.com>
2023-07-20 13:11:32 +0200

    Add task item component

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { defineProps } from "vue";
+import { Icon } from "@iconify/vue";
+const props = defineProps({
+  task: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <li>
+    <input type="checkbox" :checked="task.isCompleted" />
+    <div class="task">
+      <input v-if="task.isEditing" type="text" :value="task.task" />
+      <span v-else>
+        {{ task.task }}
+      </span>
+    </div>
+    <div class="task-actions">
+      <Icon
+        v-if="task.isEditing"
+        icon="ph:check-circle"
+        class="icon check-icon"
+        color="41b080"
+        width="22"
+      />
+      <Icon
+        v-else
+        icon="ph:pencil-fill"
+        class="icon edit-icon"
+        color="41b080"
+        width="22"
+      />
+      <Icon icon="ph:trash" class="icon trash-icon" color="f95e5e" width="22" />
+    </div>
+  </li>
+</template>
+
+<style lang="scss" scoped>
+li {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 16px 10px;
+  background-color: #f1f1f1;
+  box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1),
+    0 8px 10px -6px rgb(0 0 0 / 0.1);
+
+  &:hover {
+    .task-actions {
+      opacity: 1;
+    }
+  }
+
+  input[type="checkbox"] {
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    background-color: #fff;
+    border-radius: 50%;
+    box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+
+    &:checked {
+      background-color: #41b080;
+    }
+  }
+
+  .task {
+    flex: 1;
+
+    input[type="text"] {
+      width: 100%;
+      padding: 2px 6px;
+      border: 2px solid #41b080;
+    }
+  }
+
+  .task-actions {
+    display: flex;
+    gap: 6px;
+    opacity: 0;
+    transition: 150ms ease-in-out;
+    .icon {
+      cursor: pointer;
+    }
+  }
+}
+</style>
--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,38 +1,63 @@
 <script setup>
-  import { ref } from 'vue';
-  import {uid} from 'uid';
-  import TaskCreator from '../components/TaskCreator.vue';
-  const taskList = ref([]);
+import { ref } from "vue";
+import { uid } from "uid";
+import { Icon } from "@iconify/vue";
+import TaskCreator from "../components/TaskCreator.vue";
+import TaskItem from "../components/TaskItem.vue";
+const taskList = ref([]);
 
-  const createTask = (task) => {
-    taskList.value.push({
-      id: uid(),
-      task,
-      isCompleted: null,
-      isEditing: null
-    })
-  }
+const createTask = (task) => {
+  taskList.value.push({
+    id: uid(),
+    task,
+    isCompleted: null,
+    isEditing: null,
+  });
+};
 </script>
 
 <template>
   <main>
     <h1>Create Task</h1>
-    <TaskCreator @create-task="createTask()" />
+    <TaskCreator @create-task="createTask" />
+    <ul class="task-list" v-if="taskList.length > 0">
+      <TaskItem v-for="task in taskList" :task="task" v-bind:key="task.id" />
+    </ul>
+    <p v-else class="task-msg">
+      <Icon icon="noto-v1:sad-but-relieved-face" />
+      <span>You have no tasks to complete! Add one!</span>
+    </p>
   </main>
 </template>
 
 <style lang="scss" scoped>
-  main {
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+
+  .todo-list {
     display: flex;
     flex-direction: column;
-    max-width: 500px;
-    width: 100%;
-    margin: 0 auto;
-    padding: 40px 16px;
-
-    h1 {
-      margin-bottom: 16px;
-      text-align: center;
-    }
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
   }
+
+  .task-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+  }
+}
 </style>
